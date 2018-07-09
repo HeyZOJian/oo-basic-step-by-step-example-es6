8d61df8dd2e1163b510c81dc43a245cd0a79e1e8
vito zhuang <vito.z.j.zhuang@oocl.com>
2018-07-09 15:48:10 +0800

    feat: practice6 done

--- src/practice_6/teacher.js
+++ src/practice_6/teacher.js
@@ -1,1 +1,21 @@
+var Person = require('./person')
 
+class Teacher extends Person {
+
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    introduce() {
+        let introduceStr = super.introduce();
+        if (!this.klass) {
+            this.klass = 'No Class'
+        } else {
+            this.klass = 'Class ' + this.klass
+        }
+        introduceStr += ` I am a Teacher. I teach ${this.klass}.`;
+        return introduceStr;
+    }
+}
+module.exports = Teacher
