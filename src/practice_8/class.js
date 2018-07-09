fbe82157c709af11a568294f796267f645bf475b
vito zhuang <vito.z.j.zhuang@oocl.com>
2018-07-09 16:45:18 +0800

    feat: practice8 done

--- src/practice_8/class.js
+++ src/practice_8/class.js
@@ -1,1 +1,22 @@
+class Class {
+    constructor(number) {
+        this.number = number;
+    }
+    getDisplayName() {
+        return 'Class ' + this.number;
+    }
 
+    assignLeader(student) {
+        if (student.getKlassNumber() === this.number)
+            this.leader = student;
+    }
+
+    getLeaderName() {
+        if (this.leader)
+            return this.leader.name;
+        else
+            return null
+    }
+}
+
+module.exports = Class
