fb3f83b4b443be25661ab7962be4b9adc6b19b52
vito zhuang <vito.z.j.zhuang@oocl.com>
2018-07-09 15:47:53 +0800

    feat: practice4 done

--- src/practice_4/student.js
+++ src/practice_4/student.js
@@ -1,1 +1,15 @@
+var Person = require('./person')
+class Student extends Person {
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klass = klass;
+    }
 
+    introduce() {
+        let introduceStr = super.introduce()
+        introduceStr += ` I am a Student. I am at Class ${this.klass}.`
+        return introduceStr;
+    }
+}
+
+module.exports = Student
--- src/practice_4/worker.js
+++ src/practice_4/worker.js
@@ -1,1 +1,15 @@
+var Person = require('./person')
 
+class Worker extends Person {
+    constructor(name, age) {
+        super(name, age)
+    }
+
+    introduce() {
+        let introduceStr = super.introduce()
+        introduceStr += " I am a Worker. I have a job."
+        return introduceStr;
+    }
+}
+
+module.exports = Worker
