adeb595a394cc27f3c56c1d85796cbd6cd512826
vito zhuang <vito.z.j.zhuang@oocl.com>
2018-07-09 15:54:58 +0800

    feat: practice7 done

--- src/practice_7/student.js
+++ src/practice_7/student.js
@@ -1,1 +1,15 @@
+var Person = require('./person')
+class Student extends Person {
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klass = klass;
+    }
 
+    introduce() {
+        let introduceStr = super.introduce()
+        introduceStr += ` I am a Student. I am at ${this.klass.getDisplayName()}.`
+        return introduceStr;
+    }
+}
+
+module.exports = Student
--- src/practice_7/teacher.js
+++ src/practice_7/teacher.js
@@ -1,1 +1,30 @@
+var Person = require('./person')
 
+class Teacher extends Person {
+
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    introduce() {
+        let introduceStr = super.introduce();
+        if (this.klass)
+            introduceStr += ` I am a Teacher. I teach ${this.klass.getDisplayName()}.`;
+        else
+            introduceStr += ` I am a Teacher. I teach No Class.`;
+        return introduceStr;
+    }
+
+    introduceWith(student) {
+        let introduceWithStr = super.introduce();
+        introduceWithStr += ` I am a Teacher.`
+        if (student.klass.getDisplayName() === this.klass.getDisplayName()) {
+            introduceWithStr += ` I teach ${student.name}.`;
+        } else {
+            introduceWithStr += ` I don't teach ${student.name}.`
+        }
+        return introduceWithStr;
+    }
+}
+module.exports = Teacher
